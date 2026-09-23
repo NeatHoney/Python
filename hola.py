66943a61a9a2b57a48f2650ee48c7a95af226cd1
Francisco Herrera <fhercar3008@g.educaand.es>
2026-09-23 16:18:32 +0200

    Hecho hoy  hola.py

--- hola.py
+++ hola.py
@@ -4,4 +4,30 @@ diccionario = {
     "edad" : "18"
 }
 
-print(diccionario)
+print(diccionario["edad"]) #Así solo muestra ese valor
+print(diccionario) # Así muestra todo
+diccionario["edad"] = 20 # Así cambias el valor
+
+diccionario["direccion"] = "Calle Juan Carlos I" # Así añades un valor más a la lista
+print(diccionario["direccion"])
+print(diccionario) # Aquí se ve al final ya añadido
+
+
+
+estudiante = [
+    {
+        "nombre" : "Joselin",
+        "apellido" : "Flores",
+        "modulos" : ["Acceso a datos", "Python", "Proyecto"]
+    },
+
+    {
+        "nombre" : "Camilo",
+        "apellido" : "Sesto",
+        "modulos" : ["Acceso a datos", "Desarrollo de interfaces"]
+    }
+]
+
+
+print(estudiante[0]["modulos"][0])
+print(estudiante[1]["apellido"])
